acbdea50c3346eac37d7622887531eb73e04df19
Maarten van den Berg <maartenberg1+git@gmail.com>
2016-03-15 18:13:18 +0100

    Bump version

--- code/daemon/setup.py
+++ code/daemon/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='infozuild',
-    version='0.3.1',
+    version='0.4.0',
     description='daemon and scripts for updating the infozuil',
 
     url='https://github.com/svsticky/information-column',
